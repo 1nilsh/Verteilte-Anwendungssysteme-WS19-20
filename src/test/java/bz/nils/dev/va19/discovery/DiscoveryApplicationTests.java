4545713fee39086fecdf6c2f9ce23dc61e8516dc
Nils Hodys <dev@pmail.nils.bz>
2020-02-10 12:10:23 +0100

    [master] setup standard eureka server from spring initializr

---
+++ src/test/java/bz/nils/dev/va19/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package bz.nils.dev.va19.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
