1c33f0d06f65c277fa3e790291979ffe08fee0b4
Eike Schweighoefer <eike.schweighoefer@stud.leuphana.de>
2020-03-27 17:55:40 +0100

    Refacture Structure REST-Endpoints für Order Register with the DiscoveryService

--- src/main/java/bz/nils/dev/va19/order/connector/OrderRestConnectorProvider.java
+++ src/main/java/bz/nils/dev/va19/order/connector/OrderRestConnectorProvider.java
@@ -5,7 +5,11 @@ import bz.nils.dev.va19.order.component.structure.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/order")
+@RestController
 public class OrderRestConnectorProvider {
     private final OrderService orderService;
 
--- src/test/java/bz/nils/dev/va19/order/OrderApplicationTests.java
+++ src/test/java/bz/nils/dev/va19/order/OrderApplicationTests.java
@@ -1,13 +1,13 @@
 package bz.nils.dev.va19.order;
 
-		import org.junit.jupiter.api.Test;
-				import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
