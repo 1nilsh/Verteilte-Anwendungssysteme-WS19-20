f89fba30ff30be752b7913da8b15b742406589e4
nils.hodys <nils.hodys@stud.leuphana.de>
2020-03-30 23:41:01 +0200

    [master] gs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:11
 ADD target/article-microservice.jar article-microservice.jar
+ADD service.properties service.properties
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "article-microservice.jar"]
+ENTRYPOINT ["java", "-jar", "article-microservice.jar", "--spring.config.location=/service.properties"]
--- src/test/java/bz/nils/dev/va19/article/ArticleApplicationTests.java
+++ src/test/java/bz/nils/dev/va19/article/ArticleApplicationTests.java
@@ -2,14 +2,15 @@ package bz.nils.dev.va19.article;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class ArticleApplicationTests {
 
+
+
     @Test
-    void isArticleCreated() throws Exception {
-        Assert.isTrue(true);
+    void contextLoads() {
     }
+
 
 }
